35770f0012d2e9e8cb6b7d6ca11e884097aa1d3e
Son Nguyen <sonnguyen201.dev@gmail.com>
2023-01-15 23:00:01 +0700

    add node version file

--- src/app.js
+++ src/app.js
@@ -29,4 +29,4 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`🚀 @ http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`🚀 @ Server run on port: ${PORT}`));
