dd5b072d9b2f88114d1de4b863f5e11cfd882259
Son Nguyen <sonnguyen201.dev@gmail.com>
2023-01-15 23:36:46 +0700

    rm dist

--- dist/src/routes/v1/chapter.route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports["default"] = void 0;
-var route = require('express').Router();
-var _default = route;
-exports["default"] = _default;
